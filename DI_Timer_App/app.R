6f3351e709e73abefefb7985404e7e4d1e03beae
Jason G <jasondouglasgibbs@gmail.com>
2022-07-21 16:44:41 -0400

    -Added Haunted Carriage info, removed some redundant code.

--- DI_Timer_App/app.R
+++ DI_Timer_App/app.R
@@ -3,8 +3,6 @@ library(readxl)
 library(lubridate)
 library(hms)
 library(shinydashboard)
-library(kableExtra)
-library(shinyjs)
 TimeTable<-read_xlsx("DI_Lookup_Table.xlsx")
 
 ##Defines the UI.##
@@ -16,15 +14,13 @@ ui <- dashboardPage(skin="green",
                     ##Sidebar with inputs.##
                     dashboardSidebar(width=350,
                                      sidebarMenu(
-                                       selectInput("server", "Server:", TimeTable$Server_Name),
-                                       actionButton("submit", "Submit")
-                                     )
+                                       selectInput("server", "Server:", TimeTable$Server_Name))
                                      
                     ),
                     
                     ##Body outputs.##
-                    dashboardBody(shinyjs::useShinyjs(),#tableOutput("TimeTable"),
-                                  tableOutput("AncientNightmare12PM"),
+                    dashboardBody(#tableOutput("TimeTable"),
+                                  tableOutput("AncientNightmare12PM")
                     )
 )
 
@@ -37,6 +33,9 @@ server <- function(input, output, session) {
     TimeTable$Ancient_Nightmare_12PM<-as.POSIXct(TimeTable$Ancient_Nightmare_12PM, tz="UTC")
     TimeTable$Ancient_Nightmare_830PM<-as.POSIXct(TimeTable$Ancient_Nightmare_830PM, tz="UTC")
     TimeTable$Ancient_Nightmare_10PM<-as.POSIXct(TimeTable$Ancient_Nightmare_10PM, tz="UTC")
+    TimeTable$Haunted_Carriage_12PM<-as.POSIXct(TimeTable$Haunted_Carriage_12PM, tz="UTC")
+    TimeTable$Haunted_Carriage_830PM<-as.POSIXct(TimeTable$Haunted_Carriage_830PM, tz="UTC")
+    TimeTable$Haunted_Carriage_10PM<-as.POSIXct(TimeTable$Haunted_Carriage_10PM, tz="UTC")
     TimeTable$System_Time<-now()
     TimeTable$UTC_Time<-now("UTC")
     TimeTable<-as.data.frame(TimeTable)
@@ -70,6 +69,27 @@ server <- function(input, output, session) {
       TimeTable[i, "Ancient_Nightmare_10PM"]<-as.POSIXct(paste0(date(TimeTable[i, "Ancient_Nightmare_10PM"])," ", "22:00:00"), tz='UTC')
       
       
+      ##Haunted Carriage##
+      HCDate<-weekdays(TimeTable[i, "Server_Time"])
+      HCDay<-TimeTable[i,"Server_Time"]
+      while(HCDate!="Tuesday"&&HCDate!="Saturday"){
+        HCDay<-HCDay+days(1)
+        HCDate<-weekdays(HCDay)
+      }
+      
+      TimeTable[i, "Haunted_Carriage_12PM"]<-force_tz(HCDay,'UTC')
+      TimeTable[i, "Haunted_Carriage_12PM"]<-as.POSIXct(paste0(date(TimeTable[i, "Haunted_Carriage_12PM"])," ", "12:00:00"), tz='UTC')
+      
+      TimeTable[i, "Haunted_Carriage_830PM"]<-force_tz(HCDay,'UTC')
+      TimeTable[i, "Haunted_Carriage_830PM"]<-as.POSIXct(paste0(date(TimeTable[i, "Haunted_Carriage_830PM"])," ", "20:30:00"), tz='UTC')
+      
+      TimeTable[i, "Haunted_Carriage_10PM"]<-force_tz(HCDay,'UTC')
+      TimeTable[i, "Haunted_Carriage_10PM"]<-as.POSIXct(paste0(date(TimeTable[i, "Haunted_Carriage_10PM"])," ", "22:00:00"), tz='UTC')
+      
+      
+      
+      
+      
     }
     
     # output$TimeTable<- renderText({
@@ -91,6 +111,7 @@ server <- function(input, output, session) {
     }
     
     
+    ##Ancient Nightmare.##
     countdowntime<-as.character(round_hms(as_hms(difftime(TimeTable[1, "Ancient_Nightmare_12PM"], TimeTable[1,"Server_Time"])), digits=0))
     if(grepl("-",countdowntime)){
       countdowntime<-"0"
@@ -117,8 +138,44 @@ server <- function(input, output, session) {
     }
     
     TimerDisplayTable[3,2]<-countdowntime
+
     
+    
+    ##Haunted Carriage##
+    countdowntime<-as.character(round_hms(as_hms(difftime(TimeTable[1, "Haunted_Carriage_12PM"], TimeTable[1,"Server_Time"])), digits=0))
+    if(grepl("-",countdowntime)){
+      countdowntime<-"0"
+    }else{
+      countdowntime<-as.character(round_hms(as_hms(difftime(TimeTable[1, "Haunted_Carriage_12PM"], TimeTable[1,"Server_Time"])), digits=0))
+    }
+    
+    TimerDisplayTable[4,2]<-countdowntime
+    
+    
+    countdowntime<-as.character(round_hms(as_hms(difftime(TimeTable[1, "Haunted_Carriage_830PM"], TimeTable[1,"Server_Time"])), digits=0))
+    if(grepl("-",countdowntime)){
+      countdowntime<-"0"
+    }else{
+      countdowntime<-as.character(round_hms(as_hms(difftime(TimeTable[1, "Haunted_Carriage_830PM"], TimeTable[1,"Server_Time"])), digits=0))
+    }
+    
+    TimerDisplayTable[5,2]<-countdowntime
+    
+    
+    countdowntime<-as.character(round_hms(as_hms(difftime(TimeTable[1, "Haunted_Carriage_10PM"], TimeTable[1,"Server_Time"])), digits=0))
+    if(grepl("-",countdowntime)){
+      countdowntime<-"0"
+    }else{
+      countdowntime<-as.character(round_hms(as_hms(difftime(TimeTable[1, "Haunted_Carriage_10PM"], TimeTable[1,"Server_Time"])), digits=0))
+    }
+    
+    TimerDisplayTable[6,2]<-countdowntime
+    
+    
+    #Ordering and preping for display.##
     TimerDisplayTable<-as.data.frame(filter(TimerDisplayTable, TimerDisplayTable$Countdown!="0"))
+    TimerDisplayTable<-TimerDisplayTable[order(TimerDisplayTable$Countdown, decreasing=FALSE),]
+    
     
     
     # isolate({
